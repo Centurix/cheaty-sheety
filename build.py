e8739fd63900e45fbc578edd8f2d0ddc47fa36ed
Centurix <centurix@gmail.com>
2024-05-03 14:44:42 +0000

    Deploying to gh-pages from @ Centurix/cheaty-sheety@ea14b94f0c6ee561983d4681fbd706c9eff9100b 🚀

---
+++ build.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os
+import json
+"""
+Create the build directory and index.json file
+"""
+
+def main():
+    with open("index.json", "w") as index_handle:
+        json.dump(
+            {
+                "sheets": [entry.name for entry in os.scandir(".") if entry.is_dir()]
+            },
+            index_handle
+        )
+
+
+if __name__ == "__main__":
+    main()
